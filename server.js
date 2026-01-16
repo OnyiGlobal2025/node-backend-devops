5cde6949569b244f5835c6372a0ae576a45567b2
OnyiGlobal2025 <onyedikaokoro8@gmail.com>
2026-01-16 08:59:29 +0100

    Initial Node.js backend setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Node.js backend is running 🚀");
+});
+
+// Health check (DevOps standard)
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
